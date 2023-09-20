2a71668f7e90a3e6d6b20c0f4cfa2917ad45fb35
Garrett-Bodley <garrett.bodley@gmail.com>
2023-09-19 22:19:19 -0400

    Make Dicts, build out A and C instruction parsing.

--- 06/assembler.js
+++ 06/assembler.js
@@ -4,15 +4,74 @@ const PATH = "add/Add.asm";
 const fs = require("node:fs");
 const path = require("node:path");
 const readline = require("node:readline");
-const dict = {};
+
+const ALUDict = {
+  0: "0101010",
+  1: "0111111",
+  "-1": "0111010",
+  D: "0001100",
+  A: "0110000",
+  "!D": "0001101",
+  "!A": "0110001",
+  "-D": "0001111",
+  "-A": "0110011",
+  "D+1": "0011111",
+  "1+D": "0011111",
+  "A+1": "0110111",
+  "1+A": "0110111",
+  "D-1": "0001110",
+  "A-1": "0110010",
+  "D+A": "0000010",
+  "A+D": "0000010",
+  "D-A": "0010011",
+  "A-D": "0000111",
+  "D&A": "0000000",
+  "A&D": "0000000",
+  "D|A": "0010101",
+  "A|D": "0010101",
+
+  M: "1110000",
+  "!M": "1110001",
+  "-M": "1110011",
+  "M+1": "1110111",
+  "M-1": "1110010",
+  "D+M": "1000010",
+  "M+D": "1000010",
+  "D-M": "1010011",
+  "M-D": "1000111",
+  "D&M": "1000000",
+  "M&D": "1000000",
+  "D|M": "1010101",
+  "M|D": "1010101",
+};
+
+const destDict = {
+  M: "001",
+  D: "010",
+  MD: "011",
+  A: "100",
+  AM: "101",
+  AD: "110",
+  AMD: "111",
+};
+
+const jumpDict = {
+  JGT: "000",
+  JEQ: "010",
+  JGE: "011",
+  JLT: "100",
+  JNE: "101",
+  JLE: "111",
+};
+
+const dict = { ALUDict, destDict, jumpDict };
 
 function Assemble(relativePath) {
-  debugger;
   const rl = setupIO(relativePath);
-  debugger;
+  let lineNum = 1;
   rl.on("line", (input) => {
-    debugger;
-    parseInstruction(input);
+    parseInstruction(input, lineNum);
+    lineNum++;
   });
 
   function setupIO(relativePath) {
@@ -44,29 +103,99 @@ function Assemble(relativePath) {
     return [inPath, outPath];
   }
 
-  const parseInstruction = (line) => {
-    debugger;
+  const parseInstruction = (line, lineNum) => {
     if (line.match(/(^\/\/)/)) {
       console.log(line);
       console.log("Skipping code comment");
       return;
     }
-    if(line.length == 0){
-      return console.log("Skipping empty line!")
+    if (line.length == 0) {
+      return console.log("Skipping empty line!");
     }
     if (line.match(/@/)) {
-      aInstruction(line);
+      aInstruction(line, lineNum);
     } else {
-      cInstruction(line);
+      cInstruction(line, lineNum);
     }
   };
 
-  function aInstruction(line){
-    console.log(`The following is an A instruction:\n${line}\n-------------------------------------`)
+  function aInstruction(line, lineNum) {
+    const val = Number(line.replace(/@/, ""));
+    const binaryInstruction = "1" + twosCompliment(val, 15);
+    debugger;
+    console.log(
+      `A instruction:\n${line}\nTranslated to 16 bit Binary: \n${binaryInstruction}\n-------------------------------`
+    );
+    return binaryInstruction
   }
 
-  function cInstruction(line){
-    console.log(`The following is a C instruction:\n${line}\n-------------------------------------`)
+  function cInstruction(line, lineNum) {
+    // Example instruction code:
+    // ixxaccccccdddjjj
+    // syntax: Destination = Computation
+    //
+    // if line.includes(';')
+    //   Jump instructions
+    //   ALU outputs D Register
+    //   Hash Jump instructions
+    // else if line.includes('=')
+    //   Computation
+
+    // Split on "="
+    // Program destination component = Computation/ALU instructions
+    debugger;
+    let compBinary;
+    let destBinary = "000";
+    let jumpBinary = "000";
+    if (line.match(/=/)) {
+      const [destination, computation] = line.split("=");
+
+      if (!dict.destDict[destination] || !dict.ALUDict[computation])
+        throwInvalidInput(lineNum);
+
+      destBinary = dict.destDict[destination];
+      compBinary = dict.ALUDict[computation];
+    } else if (line.match(/;/)) {
+      const [computation, jump] = line.split(";");
+
+      if (!dict.ALUDict[computation] || !dict.jumpDict[jump])
+        throwInvalidInput(lineNum);
+
+      compBinary = dict.ALUDict[computation];
+      jumpBinary = dict.jumpDict[jump];
+    } else {
+      throwInvalidInput(lineNum);
+    }
+    let binaryInstruction = `100${compBinary}${destBinary}${jumpBinary}`;
+    console.log(
+      `C instruction:\n${line}\nTranslated to 16 bit Binary: \n${binaryInstruction}\n-------------------------------`
+    );
+    return binaryInstruction
+  }
+
+  function throwInvalidInput(lineNum){
+    throw Error(`Invalid instruction ${lineNum !== null ? `on line ${lineNum}` : ''}`);
+  }
+
+  function twosCompliment(value, bitCount) {
+    // Borrowed from Brandon Sará's gist. Thank you Brandon!
+    // https://gist.github.com/bsara/519df5f91833d01c20ec
+
+    let binaryStr;
+    if (value >= 0) {
+      let twosComp = value.toString(2);
+      binaryStr = padAndChop(twosComp, "0", bitCount || twosComp.length);
+    } else {
+      binaryStr = (Math.pow(2, bitCount) + value).toString(2);
+    }
+
+    if (Number(binaryStr) < 0) return undefined;
+
+    return binaryStr;
+  }
+
+  function padAndChop(str, padChar, length) {
+    return (Array(length).fill(padChar).join("") + str).slice(length * -1);
   }
 }
 
