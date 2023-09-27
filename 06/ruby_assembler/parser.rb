315f7599e54a219bd604dc37f5deda5debd2044d
Garrett-Bodley <garrett.bodley@gmail.com>
2023-09-27 15:13:56 -0400

    Appease rubocop - parser.rb

--- 06/ruby_assembler/parser.rb
+++ 06/ruby_assembler/parser.rb
@@ -1,5 +1,8 @@
-class Parser
+# rubocop:disable Style/RedundantReturn
+# frozen_string_literal: true
 
+# Class to parse an assembly file
+class Parser
   attr_reader :current_line
 
   def initialize(path)
@@ -8,42 +11,44 @@ class Parser
   end
 
   def command_type
-    return :comment if @current_line.match?(/(^\/\/)/)
-    return :empty if @current_line.length == 0
+    return :comment if @current_line.match?(%r{(^//)})
+    return :empty if @current_line.empty?
     return :a_command if @current_line.include?('@')
     return :l_command if @current_line.match?(/^\([A-Za-z_.$:][A-Za-z0-9_.$:]*\)$/)
-    return :c_command if @current_line.match(/[=;]/)
+    return :c_command if @current_line.match(/[=;]/) # rubocop:disable Layout/EmptyLineAfterGuardClause
     return :unknown
   end
 
   def extract_symbol(line)
     command_type = self.command_type
-    return line.sub(/@/, "") if command_type == :a_command && !self.a_command_is_const?
-    return line.gsub(/[\(\)]/, "") if command_type == :l_command
+    return line.sub(/@/, '') if command_type == :a_command && !a_command_is_const?
+    return line.gsub(/[()]/, '') if command_type == :l_command
+
     return nil
   end
 
   def symbol
-    self.extract_symbol(@current_line)
+    extract_symbol(@current_line)
   end
 
-  def has_more_commands?
+  def more_commands?
     !@file.eof?
   end
 
   def advance
     line = @file.readline(chomp: true)
-    @current_line = self.sanitize_line(line)
-    self
+    @current_line = sanitize_line(line)
   end
 
   def remove_trailing_comments(line)
-    line.sub(/(?<=\S)\s*\/\/.*$/, "")
+    line.sub(%r{(?<=\S)\s*//.*$}, '')
   end
 
   def sanitize_line(line)
-    throw ArgumentError.new(`invalid argument type (expected String, got #{line.class})`) unless line.class == String
-    self.remove_trailing_comments(line.strip)
+    unless line.instance_of?(String)
+      throw ArgumentError.new(`Invalid argument type (expected String, got #{line.class})`)
+    end
+    remove_trailing_comments(line.strip)
   end
 
   def rewind
@@ -51,31 +56,43 @@ class Parser
   end
 
   def a_command_to_b
-    binary_val = Integer(@current_line.sub(/@/, "")).to_s(2).rjust(15, "0")
+    binary_val = Integer(@current_line.sub(/@/, '')).to_s(2).rjust(15, '0')
     return binary_val
   end
 
   def a_command_is_const?
-    return StandardError.new("current_line is an invalid instruction type (expected :a_command or :l_command, got #{self.command_type.to_s})") unless [:a_command, :l_command].include?(self.command_type)
-    !!self.current_line.match?(/^@\d+$/)
+    unless %i[a_command l_command].include?(command_type)
+      return StandardError.new("current_line is an invalid instruction type (expected :a_command or :l_command, got #{command_type})") # rubocop:disable Layout/LineLength
+    end
+
+    !!current_line.match?(/^@\d+$/)
   end
 
   def comp
-    return StandardError.new("current_line is of invalid instruction type  (expected :c_command, got #{self.command_type})") unless self.command_type == :c_command
-    return self.current_line.split("=").last if(self.current_line.include?("="))
-    return self.current_line.split(";").first if self.current_line.include?(";")
+    unless command_type == :c_command
+      return StandardError.new("current_line is of invalid instruction type  (expected :c_command, got #{command_type})")
+    end
+    return current_line.split('=').last if(current_line.include?('='))
+    return current_line.split(';').first if current_line.include?(';')
   end
 
-
   def jump
-    return StandardError.new("current_line is an invalid instruction type (expected :c_command, got #{self.command_type})") unless self.command_type == :c_command
-    return self.current_line.split(";").last if self.current_line.include?(";")
-    return ""
+    unless command_type == :c_command
+      return StandardError.new("current_line is an invalid instruction type (expected :c_command, got #{command_type})")
+    end
+    return current_line.split(';').last if current_line.include?(';')
+
+    return ''
   end
 
   def dest
-    return StandardError.new("current_line is an invalid instruction type (expected :c_command, got #{self.command_type})") unless self.command_type == :c_command
-    return self.current_line.split("=").first if self.current_line.include?("=")
-    return ""
+    unless command_type == :c_command
+      return StandardError.new("current_line is an invalid instruction type (expected :c_command, got #{command_type})")
+    end
+    return current_line.split('=').first if current_line.include?('=')
+
+    return ''
   end
 end
+
+# rubocop:enable Style/RedundantReturn
