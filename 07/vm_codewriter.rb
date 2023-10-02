3cb8ff98e9f2468f0865c75e312411a99d78106a
Garrett-Bodley <garrett.bodley@gmail.com>
2023-10-02 13:58:09 -0400

    Second Draft. Passes StackTest

--- 07/vm_codewriter.rb
+++ 07/vm_codewriter.rb
@@ -4,11 +4,21 @@
 # Writes VM Code
 class VMCodeWriter
   ARITHMETIC_OPTIONS = %w[add sub neg eq gt lt and or not].freeze
+  SYMBOL_TABLE = {
+    eq: 'JEQ',
+    lt: 'JLT',
+    gt: 'JGT',
+    add: '+',
+    sub: '-',
+    and: '&',
+    or: '|',
+    neg: '-',
+    not: '!'
+  }.freeze
 
   def initialize(path)
-    binding.pry
     @output_file = File.open(path, 'w')
-    @label_basename = path.basename('.*').to_s
+    @basename = path.basename('.*').to_s
     @label_count = 0
   end
 
@@ -18,7 +28,7 @@ class VMCodeWriter
   end
 
   def write_push(segment, val)
-    write_constant if segment == :constant
+    write_constant(val) if segment == :constant
   end
 
   def write_constant(val)
@@ -37,41 +47,79 @@ class VMCodeWriter
   end
 
   def write_arithmetic(string)
-    case string.to_sym
-    when :add
-      write_add
-    when :sub
-      write_sub
+    string_sym = string.to_sym
+    case string_sym
+    when :add, :sub
+      write_add_subtract(SYMBOL_TABLE[string_sym])
+    when :neg, :not
+      write_neg_not(SYMBOL_TABLE[string_sym])
+    when :eq, :lt, :gt
+      write_arithmetic_comparison(SYMBOL_TABLE[string_sym])
+    when :and, :or
+      write_and_or(SYMBOL_TABLE[string_sym])
     end
   end
 
-  def write_add
-    %w[@SP AM=M-1 D=M A=A-1 M=M+D].each do |assembly_instruction|
-      output_file.puts assembly_instruction
+  def write_add_subtract(operator)
+    [
+      '@SP',
+      'AM=M-1',
+      'D=M',
+      'A=A-1',
+      "M=M#{operator}D"
+    ].each do |instruction|
+      @output_file.puts instruction
     end
   end
 
-  def write_sub
-    %w[@SP AM=M-1 D=M A=A-1 M=M-D].each do |command|
+  def write_arithmetic_comparison(comp_type)
+    [
+      '@SP',
+      'AM=M-1',
+      'D=M',
+      'A=A-1',
+      'D=M-D',
+      "@#{@basename}.#{@label_count}",
+      "D;#{comp_type}",
+      '@0',
+      'D=A',
+      "@#{@basename}.#{@label_count + 1}",
+      '0;JMP',
+      "(#{@basename}.#{@label_count})",
+      '@0',
+      'A=A-1',
+      'D=A',
+      "(#{@basename}.#{@label_count + 1})",
+      '@SP',
+      'A=M-1',
+      'M=D'
+    ].each do |command|
+      @output_file.puts command
+    end
+    @label_count += 2
+  end
+
+  def write_and_or(operator)
+    [
+      '@SP',
+      'AM=M-1',
+      'D=M',
+      'A=A-1',
+      "M=D#{operator}M"
+    ].each do |command|
       @output_file.puts command
     end
   end
 
-  def write_eq
-    %w[@SP A=M-1 D=M A=A-1]
-
-    # @SP
-    # AM=M-1
-    # D=M
-    # A=A-1
-    # M=M-D
-    # 
-
-    # (SET_TRUE)
-
-    # (SET_FALSE)
-
-
+  def write_neg_not(operator)
+    @output_file.puts '// WRITING NEG'
+    [
+      '@SP',
+      'A=M-1',
+      "M=#{operator}M"
+    ].each do |command|
+      @output_file.puts command
+    end
   end
 
   def write_infinite_loop
