d59338be4c37190bd4b70de0d976287b639138d8
Garrett Bodley <garrett.bodley@gmail.com>
2023-10-21 16:59:30 -0400

    Optimize Jack code output

--- 09/png_converter.rb
+++ 09/png_converter.rb
@@ -30,15 +30,38 @@ word = []
   end
 end
 
-words_as_strings = words.map.with_index do |word, i|
+twos_comp_arr = words.map do |word|
   int = word.reverse.join.to_i(2)
   twos_comp = int > 32_767 ? int - 65_536 : int
   twos_comp = '32767 + 1' if twos_comp == -32_768
-  word.any?(1) ? "    do Memory.poke(memAddr + #{i}, #{twos_comp});" : ''
+  twos_comp
+  # word.any?(1) ? "    do Memory.poke(memAddr + #{i}, #{twos_comp});" : ''
 end
 
-words_as_strings.delete_if(&:empty?)
+instruction_arr = []
+i = 0
+while i < twos_comp_arr.length
+  num = twos_comp_arr[i]
+  if num == 0
+    instruction_arr << ''
+  elsif num != twos_comp_arr[i + 1]
+    instruction_arr << "    do Memory.poke(memAddr + #{i}, #{num});"
+  else
+    first = i
+    i += 1 while twos_comp_arr[i] == twos_comp_arr[i + 1]
+    last = i
 
+    loop = %(    let i = #{first};
+    while(i < #{last + 1}){
+      do Memory.poke(memAddr + i, #{num});
+      let i = i + 1;
+    })
+    instruction_arr << loop
+  end
+  i += 1
+end
+
+instruction_arr.delete_if(&:empty?)
 class_name = out_path.basename('.*').to_s
 content = %(
 
@@ -48,9 +71,9 @@ class #{class_name}{
   }
 
   function void draw(){
-    var int memAddr;
+    var int i, memAddr;
     let memAddr = 16384;
-#{words_as_strings.join("\n")}
+#{instruction_arr.join("\n")}
     return;
   }
 }
