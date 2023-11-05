c6ccba3c357658c2a15a88f33358e6f0ddc13617
Garrett Bodley <garrett.bodley@gmail.com>
2023-11-05 15:43:31 -0500

    Compiler AST generation works! :D

--- 10/compilation_engine.rb
+++ 10/compilation_engine.rb
@@ -1,4 +1,4 @@
-# rubocop:disable Metrics/AbcSize
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/ClassLength
 # frozen_string_literal: true
 
 require_relative './compilation_dictionary'
@@ -33,6 +33,10 @@ class CompilationEngine
     compile_class_var_dec while @current_token.string.match?(/static|field/)
 
     compile_subroutine while @current_token.string.match?(/constructor|method|function/)
+
+    expect(@current_token.string == '}')
+    write_token_and_advance
+
     @space_count -= 2
     write_string('</class>')
     @block.pop
@@ -90,6 +94,13 @@ class CompilationEngine
     compile_param_list
 
     expect(@current_token.string == ')')
+    write_token_and_advance
+
+    compile_subroutine_body
+
+    @space_count -= 2
+    write_string('</subroutineDec>')
+    @block.push('Subroutine Declaration')
 
   end
 
@@ -99,6 +110,60 @@ class CompilationEngine
     @block.push('Parameter List')
     write_string('<parameterList>')
     @space_count += 2
+
+    unless @current_token.string == ')'
+      expect(@current_token.string.match?(/void|int|char|boolean/) || @current_token.type == 'IDENTIFIER')
+      write_token_and_advance
+
+      expect(@current_token.type == 'IDENTIFIER')
+      write_token_and_advance
+
+      while @current_token.string == ','
+        write_token_and_advance
+        expect(@current_token.string.match?(/void|int|char|boolean/) || @current_token.type == 'IDENTIFIER')
+        write_token_and_advance
+        expect(@current_token.type == 'IDENTIFIER')
+        write_token_and_advance
+      end
+    end
+
+    @space_count -= 2
+    write_string('</parameterList>')
+    @block.pop
+  end
+
+  def compile_subroutine_body
+    # '{' varDec* statements '}'
+    @block.push('Subroutine Body')
+    write_string('<subroutineBody>')
+    @space_count += 2
+
+    expect(@current_token.string == '{')
+    write_token_and_advance
+
+    compile_var_dec while @current_token.string == 'var'
+
+    compile_statements
+
+    expect(@current_token.string == '}')
+    write_token_and_advance
+
+    @space_count -= 2
+    write_string('</subroutineBody>')
+    @block.pop
+  end
+
+  def compile_var_dec
+    # compiles a var declaration
+    # 'var' type varName (',' varName)* ';'
+    @block.push('Var Declaration')
+    write_string('<varDec>')
+    @space_count += 2
+
+    expect(@current_token.string == 'var')
+    write_token_and_advance
+
+    # expect type
     expect(@current_token.string.match?(/void|int|char|boolean/) || @current_token.type == 'IDENTIFIER')
     write_token_and_advance
 
@@ -107,48 +172,204 @@ class CompilationEngine
 
     while @current_token.string == ','
       write_token_and_advance
-      expect(@current_token.string.match?(/void|int|char|boolean/) || @current_token.type == 'IDENTIFIER')
-      write_token_and_advance
       expect(@current_token.type == 'IDENTIFIER')
       write_token_and_advance
     end
-    expect(@current_token.string == ')')
+
+    expect(@current_token.string == ';')
+    write_token_and_advance
+
     @space_count -= 2
-    write_string('</parameterList>')
+    write_string('</varDec>')
     @block.pop
-  end
-
-  def compile_var_dec
-    # compiles a var declaration
-  end
-
-  def compile_subroutine_body
-    # '{' varDec* statements '}'
-
   end
 
   def compile_statements
     # compiles a sequence of statements, not including the enclosing "{}"
+    # binding.pry
+
+    @block.push('Statements')
+    write_string('<statements>')
+    @space_count += 2
+
+
+    while @current_token.string != '}'
+      case @current_token.string
+      when 'do'
+        compile_do
+      when 'let'
+        compile_let
+      when 'while'
+        compile_while
+      when 'if'
+        compile_if
+      when 'return'
+        compile_return
+      end
+    end
+
+    @space_count -= 2
+    write_string('</statements>')
+    @block.pop
   end
 
   def compile_do
     # compiles a do statement
+    # binding.pry
+    @block.push('Do Statement')
+    write_string('<doStatement>')
+    @space_count += 2
+
+    expect(@current_token.string == 'do')
+    write_token_and_advance
+
+    # compile subroutine call
+    # test file wants you to write out empty expressionList tags for some reason
+    # So I have to rewrite the same logic with one minor tweek
+    expect(@current_token.type == 'IDENTIFIER')
+    write_token_and_advance
+
+    expect(@current_token.string.match?(/\(|\./))
+
+    if @current_token.string == '.'
+      write_token_and_advance
+
+      expect(@current_token.type == 'IDENTIFIER')
+      write_token_and_advance
+    end
+
+    expect(@current_token.string == '(')
+    write_token_and_advance
+
+    compile_expression_list
+
+    expect(@current_token.string == ')')
+    write_token_and_advance
+
+    # end subroutine call
+
+    expect(@current_token.string == ';')
+    write_token_and_advance
+
+    @space_count -= 2
+    write_string('</doStatement>')
+    @block.pop
+  end
+
+  def compile_subroutine_call
+    expect(@current_token.type == 'IDENTIFIER')
+    write_token_and_advance
+
+    expect(@current_token.string.match?(/\(|\./))
+
+    if @current_token.string == '.'
+      write_token_and_advance
+
+      expect(@current_token.type == 'IDENTIFIER')
+      write_token_and_advance
+    end
+
+    expect(@current_token.string == '(')
+    write_token_and_advance
+
+    compile_expression_list if @current_token.string != ')'
+
+    expect(@current_token.string == ')')
+    write_token_and_advance
   end
 
   def compile_let
     # compiles a let statement
+    @block.push('Let Statement')
+    write_string('<letStatement>')
+    @space_count += 2
+
+    expect(@current_token.string == 'let')
+    write_token_and_advance
+
+    expect(@current_token.type == 'IDENTIFIER')
+    write_token_and_advance
+
+    if @current_token.string == '['
+      write_token_and_advance
+      compile_term
+      expect(@current_token.string == ']')
+      write_token_and_advance
+    end
+
+    expect(@current_token.string == '=')
+    write_token_and_advance
+
+    compile_expression
+
+    expect(@current_token.string == ';')
+    write_token_and_advance
+
+    @space_count -= 2
+    write_string('</letStatement>')
+    @block.pop
   end
 
   def compile_while
+    open_structure_tag('While Statement', '<whileStatement>')
     # compiles a while statement
+    expect(@current_token.string == 'while')
+    write_token_and_advance
+
+    expect(@current_token.string == '(')
+    compile_expression
+    expect(@current_token.string == ')')
+    write_token_and_advance
+    expect(@current_token.string == '{')
+    compile_statements
+    expect(@current_token.string == '}')
+    write_token_and_advance
+
+    close_structure_tag('</whileStatement>')
   end
 
   def compile_return
+    open_structure_tag('Return Statement', '<returnStatement>')
     # compiles a return statement
+    expect(@current_token.string == 'return')
+    write_token_and_advance
+    compile_expression if(@current_token.string != ';')
+    expect(@current_token.string == ';')
+    write_token_and_advance
+
+    close_structure_tag('</returnStatement>')
   end
 
   def compile_if
     # compiles an if statement, possible with a a trailing else clause
+    open_structure_tag('If Statement', '<ifStatement>')
+
+
+    expect(@current_token.string == 'if')
+    write_token_and_advance
+
+    expect(@current_token.string == '(')
+    write_token_and_advance
+    compile_expression
+    expect(@current_token.string == ')')
+    write_token_and_advance
+
+    expect(@current_token.string == '{')
+    write_token_and_advance
+    compile_statements
+    expect(@current_token.string == '}')
+    write_token_and_advance
+
+    close_structure_tag('</ifStatement>') && return unless @current_token.string == 'else'
+
+    write_token_and_advance
+    expect(@current_token.string == '{')
+    write_token_and_advance
+    compile_statements
+    expect(@current_token.string == '}')
+    write_token_and_advance
+
+    close_structure_tag('</ifStatement>')
   end
 
   def compile_term
@@ -158,14 +379,122 @@ class CompilationEngine
     # Specifically, if the current token is an identifier, the routine must distinguish between a variable,
     # an array entry, and a subroutine call
 
-    # A single look-ahead token, which may be one of "[" "{" "(" or "." suffices to distinguish between the
+    # A single look-ahead token, which may be one of "[" "(" or "." suffices to distinguish between the
     # three possibilities
 
     # Any other token is not part of this term and should not be advanced over
+    @block.push('Term')
+    write_string '<term>'
+    @space_count += 2
+
+    case @current_token.type
+    when 'IDENTIFIER'
+      compile_identifier_term
+    when 'INT_CONST'
+      compile_int_const
+    when 'STRING_CONST'
+      compile_string_const
+    when 'SYMBOL'
+      case @current_token.string
+      when '('
+        compile_paren_term
+      when /-|~/
+        compile_unary_op
+      end
+    when 'KEYWORD'
+      expect(@current_token.string.match?(/true|false|this|null/))
+      compile_keyword_term
+    end
+
+    @space_count -= 2
+    write_string '</term>'
+    @block.pop
+  end
+
+  def compile_keyword_term
+    expect(@current_token.type == 'KEYWORD')
+    write_token_and_advance
+  end
+
+  def compile_int_const
+    expect(@current_token.type == 'INT_CONST')
+    write_token_and_advance
+  end
+
+  def compile_string_const
+    expect(@current_token.type == 'STRING_CONST')
+    write_token_and_advance
+  end
+
+  def compile_unary_op
+    expect(@current_token.string.match?(/-|~/))
+    write_token_and_advance
+  end
+
+  def compile_paren_term
+    # '(' expression ')'
+    expect(@current_token.string == '(')
+    write_token_and_advance
+    compile_expression
+    expect(@current_token.string == ')')
+    write_token_and_advance
+  end
+
+  def compile_identifier_term
+    case @tokenizer.peek.string
+    when '['
+      compile_array_entry
+    when /\(|\./
+      compile_subroutine_call
+    else
+      expect(@current_token.type == 'IDENTIFIER')
+      write_token_and_advance
+    end
+  end
+
+  def compile_array_entry
+    # array[expression]
+    expect(@current_token.type == 'IDENTIFIER')
+    write_token_and_advance
+
+    expect(@current_token.string == '[')
+    write_token_and_advance
+
+    compile_term
+
+    expect(@current_token.string == ']')
+    write_token_and_advance
   end
 
   def compile_expression_list
     # compiles a (possibly empty) comma-separated list of expressions
+    open_structure_tag('Expression List', '<expressionList>')
+
+    if @current_token.string != ')'
+      compile_expression
+      while @current_token.string == ','
+        write_token_and_advance
+        compile_expression
+      end
+    end
+
+    close_structure_tag('</expressionList>')
+  end
+
+  def compile_expression
+    @block.push('Expression')
+    write_string('<expression>')
+    @space_count += 2
+
+    compile_term
+    while @current_token.string.match?(%r{\+|-|\*|/|&|\||<|>|=})
+      write_token_and_advance
+      compile_term
+    end
+
+    @space_count -= 2
+    write_string('</expression>')
+    @block.pop
   end
 
   def expect(boolean)
@@ -185,6 +514,18 @@ class CompilationEngine
     @current_token = @tokenizer.advance
   end
 
+  def open_structure_tag(block, tag)
+    @block.push block
+    write_string tag
+    @space_count += 2
+  end
+
+  def close_structure_tag(tag)
+    @space_count -= 2
+    write_string tag
+    @block.pop
+  end
+
 end
 
-# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/ClassLength
