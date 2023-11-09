cbbcf9beafee6dcb8f764aff2b6ef7116d9c85a6
Garrett Bodley <garrett.bodley@gmail.com>
2023-11-09 15:19:00 -0500

    Remove logic for accessing symbol table in to_s method

--- 11/Compiler/token.rb
+++ 11/Compiler/token.rb
@@ -11,19 +11,19 @@ class Token
     @line_num = line_num
   end
 
-  def to_s(table) # rubocop:disable Metrics/MethodLength
+  def to_s # rubocop:disable Metrics/MethodLength
     case @type
     when 'INT_CONST'
       type_string = 'integerConstant'
     when 'STRING_CONST'
       type_string = 'stringConstant'
-    when 'IDENTIFIER'
-      kind = table.kind?(@string)
-      type = table.type?(@string)
-      index = table.index?(@string)
-      tag_string = [kind.to_s.downcase, type.capitalize, index].join
-      # <kind type index> content </kind type index>
-      return "<#{tag_string}> #{@string} </#{tag_string}>"
+    # when 'IDENTIFIER'
+    #   kind = table.kind?(@string)
+    #   type = table.type?(@string)
+    #   index = table.index?(@string)
+    #   tag_string = [kind.to_s.downcase, type.capitalize, index].join
+    #   # <kind type index> content </kind type index>
+    #   return "<#{tag_string}> #{@string} </#{tag_string}>"
     else
       type_string = @type.downcase
     end
